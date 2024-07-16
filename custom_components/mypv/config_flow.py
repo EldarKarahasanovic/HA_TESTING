2226edb20a298943ec6703411e46ea4795b15303
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-16 11:28:19 +0200

    Update config_flow.py

--- custom_components/mypv/config_flow.py
+++ custom_components/mypv/config_flow.py
@@ -156,7 +156,6 @@ class MypvOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
         self.config_entry = config_entry
-        self._filtered_sensor_types = filtered_sensor_types
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
