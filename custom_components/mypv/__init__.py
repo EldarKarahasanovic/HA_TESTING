75f0cf74bea015d88f867609e7954ae2f813fe73
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-17 13:56:24 +0200

    Update __init__.py

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -59,7 +59,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # Reload entry when its updated.
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
 
-
     if not coordinator.last_update_success:
         raise ConfigEntryNotReady
 
@@ -67,18 +66,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         DATA_COORDINATOR: coordinator,
     }
 
-    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
+    # Load the sensor platform
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
 
     return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
-
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
