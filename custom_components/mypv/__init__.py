7da824d7eb200ecc25489af3fcff48d1d40def2a
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-17 11:42:29 +0200

    Update __init__.py

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -1,6 +1,6 @@
 """ Integration for MYPV AC-Thor"""
 import voluptuous as vol
-
+import logging
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import (
@@ -14,6 +14,8 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, SENSOR_TYPES, DATA_COORDINATOR
 from .coordinator import MYPVDataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
 
 
 CONFIG_SCHEMA = vol.Schema(
@@ -56,6 +58,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     await coordinator.async_refresh()
 
+    # Reload entry when its updated.
+    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
     if not coordinator.last_update_success:
         raise ConfigEntryNotReady
 
@@ -67,3 +72,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     return True
 
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
