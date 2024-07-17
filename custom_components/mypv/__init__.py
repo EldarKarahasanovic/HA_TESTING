482694ed0aa4d20f502c3d9f062244a09708ecb8
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-17 11:44:10 +0200

    Update __init__.py

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -8,11 +8,17 @@ from homeassistant.const import (
     CONF_MONITORED_CONDITIONS,
 )
 import homeassistant.helpers.config_validation as cv
-
-from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.typing import HomeAssistantType
 
-from .const import DOMAIN, SENSOR_TYPES, DATA_COORDINATOR
+from .const import (
+    PLATFORMS,
+    DOMAIN,
+    SENSOR_TYPES,
+    DATA_COORDINATOR,
+)
+
 from .coordinator import MYPVDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -24,7 +30,8 @@ CONFIG_SCHEMA = vol.Schema(
             {
                 vol.Required(CONF_HOST): cv.string,
                 vol.Required(CONF_MONITORED_CONDITIONS): vol.All(
-                    cv.ensure_list, [vol.In(list(SENSOR_TYPES))]
+                    cv.ensure_list,
+                    [vol.In(list(SENSOR_TYPES))],
                 ),
             }
         )
@@ -45,17 +52,18 @@ async def async_setup(hass, config):
             DOMAIN, context={"source": SOURCE_IMPORT}, data=dict(config[DOMAIN])
         )
     )
+
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Load the saved entities."""
+
     coordinator = MYPVDataUpdateCoordinator(
         hass,
         config=entry.data,
         options=entry.options,
     )
-
     await coordinator.async_refresh()
 
     # Reload entry when its updated.
@@ -68,7 +76,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         DATA_COORDINATOR: coordinator,
     }
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch", "button"])
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
 
     return True
 
@@ -78,6 +88,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
 
 async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
