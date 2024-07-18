91246d135075b9b593e96486468db5646b0d49a4
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-18 07:36:59 +0200

    Update coordinator.py

--- custom_components/mypv/coordinator.py
+++ custom_components/mypv/coordinator.py
@@ -1,5 +1,4 @@
 """Provides the MYPV DataUpdateCoordinator."""
-
 from datetime import timedelta
 import logging
 import requests
@@ -8,7 +7,7 @@ import json
 from async_timeout import timeout
 from homeassistant.util.dt import utcnow
 from homeassistant.const import CONF_HOST
-from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import DOMAIN
@@ -19,21 +18,19 @@ _LOGGER = logging.getLogger(__name__)
 class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
     """Class to manage fetching MYPV data."""
 
-    def __init__(self, hass: HomeAssistantType, *, config: dict, options: dict) -> None:
+    def __init__(self, hass: HomeAssistant, *, config: dict, options: dict):
         """Initialize global NZBGet data updater."""
         self._host = config[CONF_HOST]
         self._info = None
         self._setup = None
-        self._firmware = None
         self._next_update = 0
-        self._next_update_firmware = 0
-        self.update_interval = timedelta(seconds=10)
+        update_interval = timedelta(seconds=10)
 
         super().__init__(
             hass,
             _LOGGER,
             name=DOMAIN,
-            update_interval=self.update_interval,
+            update_interval=update_interval,
         )
 
     async def _async_update_data(self) -> dict:
@@ -41,26 +38,18 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
 
         def _update_data() -> dict:
             """Fetch data from NZBGet via sync functions."""
-            data = self.json_update("data")
+            data = self.data_update()
             if self._info is None:
-                self._info = self.json_update("mypv_dev")
+                self._info = self.info_update()
 
             if self._setup is None or self._next_update < utcnow().timestamp():
                 self._next_update = utcnow().timestamp() + 120  # 86400
-                self._setup = self.json_update("setup")
-
-            if (
-                self._firmware is None
-                or self._next_update_firmware < utcnow().timestamp()
-            ):
-                self._next_update_firmware = utcnow().timestamp() + (7 * 86400)
-                # @todo self._firmware = self.firmware_update()
+                self._setup = self.setup_update()
 
             return {
                 "data": data,
                 "info": self._info,
                 "setup": self._setup,
-                "firmware": self._firmware,
             }
 
         try:
@@ -69,31 +58,32 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
         except Exception as error:
             raise UpdateFailed(f"Invalid response from API: {error}") from error
 
-    def set_interval(self, new_interval: int):
-        """Update polling interval."""
-        self.update_interval = timedelta(seconds=new_interval)
-
-    def json_update(self, page: str):
+    def data_update(self):
         """Update inverter data."""
         try:
-            response = requests.get(f"http://{self._host}/{page}.jsn", timeout=10)
+            response = requests.get(f"http://{self._host}/data.jsn")
             data = json.loads(response.text)
             _LOGGER.debug(data)
             return data
         except:
             pass
 
-    def firmware_update(self):
-        """read the firmware info"""
+    def info_update(self):
+        """Update inverter info."""
         try:
-            response = requests.get(
-                "https://www.my-pv.com/download/currentversion.php?sn="
-                + self._info.get("sn"),
-                timeout=10,
-            )
+            response = requests.get(f"http://{self._host}/mypv_dev.jsn")
             info = json.loads(response.text)
             _LOGGER.debug(info)
             return info
         except:
-            _LOGGER.error("Mypv update firmware failed. postpone")
-            return json.loads("{}")
+            pass
+
+    def setup_update(self):
+        """Update inverter info."""
+        try:
+            response = requests.get(f"http://{self._host}/setup.jsn")
+            info = json.loads(response.text)
+            _LOGGER.debug(info)
+            return info
+        except:
+            pass
