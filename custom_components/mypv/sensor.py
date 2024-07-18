e1e849b7c900d7a45e831ec33575bc9dffe030c3
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-18 12:50:56 +0200

    Update sensor.py

--- custom_components/mypv/sensor.py
+++ custom_components/mypv/sensor.py
@@ -17,38 +17,25 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Add an my-PV entry."""
-    coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
+    coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
+        DATA_COORDINATOR
+    ]
 
     entities = []
 
     if CONF_MONITORED_CONDITIONS in entry.options:
+    
         for sensor in entry.options[CONF_MONITORED_CONDITIONS]:
-            entities.append(MypvDevice(coordinator, sensor, entry.title))
+            entities.append(MypvDevice(coordinator, sensor, entry.title))  
     else:
         for sensor in entry.data[CONF_MONITORED_CONDITIONS]:
             entities.append(MypvDevice(coordinator, sensor, entry.title))
 
+            
     async_add_entities(entities)
 
-    # Store the entities in hass.data for later reference
-    hass.data[DOMAIN][entry.entry_id]["entities"] = entities
+    #liste löscen
 
-    # Remove entities that are no longer in the options
-    existing_entities = hass.data[DOMAIN][entry.entry_id].get("entities", [])
-    entities_to_remove = []
-
-    for entity in existing_entities:
-        if entity.type not in entry.options.get(CONF_MONITORED_CONDITIONS, []):
-            entities_to_remove.append(entity)
-
-    if entities_to_remove:
-        for entity in entities_to_remove:
-            await entity.async_remove()
-
-        # Update the list of entities stored in hass.data
-        hass.data[DOMAIN][entry.entry_id]["entities"] = [
-            entity for entity in existing_entities if entity not in entities_to_remove
-        ]
 
 class MypvDevice(CoordinatorEntity):
     """Representation of a my-PV device."""
@@ -121,11 +108,3 @@ class MypvDevice(CoordinatorEntity):
             "manufacturer": "my-PV",
             "model": self.model,
         }
-    
-    async def async_remove(self):
-        """Handle removal of entity."""
-        # Clean up resources, if any
-        # For example, if using subscriptions or external connections, unsubscribe or close them here
-
-        # Call parent async_remove method if necessary
-        await super().async_remove()
