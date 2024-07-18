642b80ee12256eea84fdf912f08f2bc0072c3752
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-18 07:50:44 +0200

    Update OptionsFlow

--- custom_components/mypv/config_flow.py
+++ custom_components/mypv/config_flow.py
@@ -161,13 +161,11 @@ class MypvOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
-            return self.async_create_entry(
-                title="",
-                data={
+            self.hass.config_entries.async_update_entry(self.config_entry, data={
                     CONF_MONITORED_CONDITIONS: user_input[CONF_MONITORED_CONDITIONS],
-                },
-            )
-
+                })
+            return self.async_create_entry(title=None, data=None)
+            
         options_schema = vol.Schema(
             {
                 vol.Required(
