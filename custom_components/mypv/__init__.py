647df80dab5c1974b84160d495986fa8d5aa9de6
EldarKarahasanovic <eldar.karahasanovic@my-pv.com>
2024-07-18 07:14:03 +0200

    Update __init__.py

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -57,8 +57,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     await coordinator.async_refresh()
 
-    # Reload entry when it's updated.
+    # Reload entry when its updated.
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
 
     if not coordinator.last_update_success:
         raise ConfigEntryNotReady
@@ -67,20 +68,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         DATA_COORDINATOR: coordinator,
     }
 
-    # Forward the setup to the sensor, switch, and button platforms.
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch", "button"])
+    
 
     return True
 
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
+
+
 async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
-    coordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
-    
-    # Update coordinator configuration and refresh data.
-    coordinator.update_config(entry.data)
-    coordinator.update_options(entry.options)
-    
-    await coordinator.async_refresh()
-
-    # Reload the entry to reflect the new configuration.
     await hass.config_entries.async_reload(entry.entry_id)
