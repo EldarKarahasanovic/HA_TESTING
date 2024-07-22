1cfadbb1d4b2be788f657e729e12f3e374171cc0
EldarKarahasanovic <eldar.karah@gmail.com>
2024-07-22 09:50:36 +0200

    Update sensor.py

--- custom_components/mypv/sensor.py
+++ custom_components/mypv/sensor.py
@@ -42,7 +42,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
         new_sensor_id = f"{entry.entry_id}_{sensor}"
         new_sensor.append(new_sensor_id)
 
-    sensors_to_remove = [entity for entity in current_sensor if entity.entity_id not in configured_sensors]
+    sensors_to_remove = [entity for entity in current_entities if entity.entity_id in current_sensor and entity.entity_id not in new_sensor]
+
 
 
     for entity in sensors_to_remove:
