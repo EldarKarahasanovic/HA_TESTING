5097b7dc89564780841138146305a0e972498d5c
EldarKarahasanovic <eldar.karah@gmail.com>
2024-07-22 10:56:27 +0200

    Update sensor.py

--- custom_components/mypv/sensor.py
+++ custom_components/mypv/sensor.py
@@ -33,7 +33,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
         if entity.platform == DOMAIN and entity.config_entry_id == entry.entry_id
     ]
 
-
     new_sensor = []
     for sensor in configured_sensors:
         new_sensor_id = f"{entry.entry_id}_{sensor}"
@@ -50,10 +49,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
         if new_sensor_id not in current_entities:
             new_entity = MypvDevice(coordinator, sensor, entry.title)
             entities_to_add.append(new_entity)
-        toggle_switch = ToggleSwitch(coordinator, entry.title)
-        entities_to_add.append(toggle_switch)
-   
-
+        
+    switch_entity = ToggleSwitch(coordinator, entry.data[CONF_HOST], entry.title)
+    if switch_entity.unique_id not in [entity.unique_id for entity in current_entities]:
+        entities_to_add.append(switch_entity)
+    
     async_add_entities(entities_to_add)
 
 class MypvDevice(CoordinatorEntity):
